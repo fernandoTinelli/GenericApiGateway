7399744cb8bd9ff2c20230ed7e407ca45f03fe77
Fernando F Tinelli <fftinelli@gmail.com>
2022-10-12 22:55:18 -0300

    add route to login

--- src/Controller/APIGatewayController.php
+++ src/Controller/APIGatewayController.php
@@ -10,7 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class APIGatewayController extends AbstractController
 {
-    #[Route('/{uri}', name: 'blog_list', requirements: ['uri' => '^.*$'])]
+    #[Route('/login', name: 'app_login')]
+    public function login(Request $request, AbstractAPIGateway $apiGateway): JsonResponse
+    {
+        return $apiGateway->authenticate($request);
+    }
+
+    #[Route('/{uri}', name: 'app_index', requirements: ['uri' => '^(?!/login).*$'])]
     public function index(Request $request, AbstractAPIGateway $apiGateway): JsonResponse
     {
         return $apiGateway->handle($request);
