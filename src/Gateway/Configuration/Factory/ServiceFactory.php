45682110e4550393ce1feac779ac3472c9e966ed
Fernando F Tinelli <fftinelli@gmail.com>
2022-10-15 15:52:22 -0300

    optional and required props service

--- src/Gateway/Configuration/Factory/ServiceFactory.php
+++ src/Gateway/Configuration/Factory/ServiceFactory.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class ServiceFactory
 {
     const PROPERTIES = [
-        'address'
+        'address' => true
     ];
 
     public static function create(array $data): Service
@@ -28,11 +28,22 @@ class ServiceFactory
     {
         $name = array_key_first($data);
 
-        foreach (self::PROPERTIES as $props) {
-            if (!array_key_exists($props, $data[$name])) {
+        // Validate the props of the route
+        foreach ($data[$name] as $props) {
+            if (!array_key_exists($props, self::PROPERTIES)) {
                 throw new HttpException(
                     Response::HTTP_BAD_GATEWAY,
-                    "Erro no arquivo de configuração dos Serviços da API Gateway: $props não encontrada"
+                    "Erro no arquivo de configuração de Servicos da API Gateway: $props não é uma propriedade válida"
+                );
+            }
+        }
+
+        // Validate the required props
+        foreach (self::PROPERTIES as $props => $required) {
+            if ($required && !array_key_exists($props, $data[$name])) {
+                throw new HttpException(
+                    Response::HTTP_BAD_GATEWAY,
+                    "Erro no arquivo de configuração de Servicos da API Gateway: $props não encontrada"
                 );
             }
         }
